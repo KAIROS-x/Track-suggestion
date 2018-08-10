834e8b713f848d3573d18f90dbc76e13387c0245
da <pandemic74@gmail.com>
2018-08-10 13:06:22 -0700

    add formating clarity in JS polished indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
-    $("#questionaire").submit(function(event){
-      var q1 = $("input#q1").val();
-      var q2 = $("input#q2").val();
-
-      $(".q1").append(q1);
-      $(".q2").append(q2);
-
-      $("#story").show();
-
-      event.preventDefault();
-
-    });
+  $("#questionaire").submit(function(event){
+    var q1 = $("input#q1").val();
+    var q2 = $("input#q2").val();
+    if (q1 == "yes")
+    $("#track").show();
+    event.preventDefault();
+    
+  });
 });
